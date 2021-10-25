8f7386de02993731fac0e45fe387a7ef9e1364bc
aro <aro.fady@gmail.com>
2021-10-25 16:13:53 +0300

    php-version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b2a57cec4abe00a19c32a69afdc7d52b55dad5a7',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b2a57cec4abe00a19c32a69afdc7d52b55dad5a7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
